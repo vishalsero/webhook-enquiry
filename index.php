ed88414b96db760264881e4013d574b64df4b7ae
vishalsero <42085997+vishalsero@users.noreply.github.com>
2018-09-17 19:13:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,41 @@ if ($method == 'POST') {
         case 'Program':
             $token = getToken();
             $params = json_decode($token);
-            $enq = sendEnquire($params->access_token, $json);
-            //echo $enq;
+            $enq = sendEnquire($params->access_token, $text);
+            echo $enq;
+            $speech = "<label>Please choose the Program</label> <select id='program' onchange='programChange(this.value)'><option value='MBA'>MBA</option>
+            <option value='MCA'>MCA</option>
+            <option value='MSC'>MSC</option>
+            </select>";
+            break;
+
+        default:
+            $speech = "Sorry, I didn't get that. Can you rephrase?";
+            break;
+    }
+
+    $textResponse = $speech;
+    $textResponse = str_replace('/\\n/g', '\n', $textResponse);
+    //$textResponse = $textResponse.replace(/\\n/g, '\n\n');
+    //$textResponse = nl2br($speech);
+    //$response = new \stdClass();
+    //$response = array('type'=>0,'speech'=>$textResponse);
+    //$response = array('messages'=>array($response));
+    //$response = array('fulfillment'=>$response);
+    //$response = array('result'=>$response);
+    $response = new \stdClass();
+    $response->speech = $speech;
+    $response->displayText = $speech;
+    $response->source = "webhook";
+    //print_r($response);
+    //die();
+    //$response->result->fulfillment->speech = textResponse;
+
+    echo json_encode($response);
+} else {
+    echo "Method not allowed";
+}
+
 function getToken() {
 
     $url = $base_url.'oauth/token';
